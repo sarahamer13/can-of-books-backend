34bc446e0db9a9369004a5248fa31bf5f354e678
Sarah Mahdi <sacred13_heart@yahoo.com>
2023-05-20 17:33:29 -0700

    backend done

--- seed.js
+++ seed.js
@@ -2,6 +2,23 @@
 
 require('dotenv').config();
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DB_URL);
+mongoose.connect(process.env.MONGODB_URL);
 
-const Books = require('./')
+const Books = require('./Model/Books');
+
+async function seed (){
+  await Books.create({
+    title: 'Green Eggs and Ham',
+    description: 'An argument about the quality of breakfast',
+    status: 'Available'
+  });
+  await Books.create({
+    title: 'Good Night Moon',
+    description: 'Young bunny says good night to objects and creatures in green-walled bedroom ',
+    status: 'Available'
+  });
+  mongoose.disconnect();
+}
+
+seed();
+
