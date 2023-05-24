17a3c4e10093e4bad351527295669693fa14b336
Christina Hollie <christinahollie7@gmail.com>
2023-05-24 15:25:06 -0700

    added handler, post, and delete

---
+++ Modules/bookHandler.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const Books = require('../Model/Books');
+
+const bookHandler = {};
+
+bookHandler.getBooks = function (request, response){
+  let queryObject = {};
+
+  if(request.query.title){
+    queryObject = {title: request.query.title};
+  }
+
+  Books.find(queryObject)
+    .then(data => response.status(200).send(data))
+    .catch(err => console.error(err));
+}
+
+bookHandler.postBooks = function (request, response, next){
+  const data = request.body;
+  Books.create(data)
+  .then(addedBook => response.status(200).send(addedBook))
+  .catch(err => next(err));
+}
+
+bookHandler.deleteBooks = function (request, response, next){
+  const {id} = request.params
+  console.log(id);
+  Books.findByIdAndDelete(id)
+    .then(deletedBooks => response.status(200).send(deletedBooks))
+    .catch(err => next(err));
+}
+
+module.exports = bookHandler;
--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require ('mongoose');
 
-const getBooks = require ('./Modules/getBooks');
+const bookHandler = require ('./Modules/bookHandler');
+
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
 
@@ -19,10 +21,13 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => console.log('Mongoose is connected'));
 
-app.get('/books', getBooks);
-
-
 app.get('/', (request, response) => response.status(200).send ('Default route working'));
+
+app.get('/books', bookHandler.getBooks);
+
+app.post('/books', bookHandler.postBooks);
+
+app.delete('/books/:id', bookHandler.deleteBooks);
 
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
