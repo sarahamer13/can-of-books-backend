73ee1da1344b6a3626752840fcc1badf42d63ab2
Sarah Mahdi <sacred13_heart@yahoo.com>
2023-06-06 22:56:56 -0700

    made update

--- server.js
+++ server.js
@@ -3,13 +3,10 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
 const verifyUser = require('./Modules/Authorize');
 
-const bookHandler = require ('./Modules/bookHandler');
-
-// Responsible for getting green light with jwt
-app.use(verifyUser);
+const bookHandler = require('./Modules/bookHandler');
 
 const app = express();
 app.use(cors());
@@ -24,7 +21,10 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => console.log('Mongoose is connected'));
 
-app.get('/', (request, response) => response.status(200).send ('Default route working'));
+// Move the verifyUser middleware here
+app.use(verifyUser);
+
+app.get('/', (request, response) => response.status(200).send('Default route working'));
 
 app.get('/books', bookHandler.getBooks);
 
@@ -33,6 +33,5 @@ app.post('/books', bookHandler.postBooks);
 app.delete('/books/:id', bookHandler.deleteBooks);
 
 app.put('/books/:id', bookHandler.updateBooks);
-
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
